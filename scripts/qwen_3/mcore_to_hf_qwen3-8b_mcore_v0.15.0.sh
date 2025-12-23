5823226ba29da6f972c31e1f46044cc42249059b
kazuki <kaz.tokyo.tech20@gmail.com>
2025-12-23 20:56:08 +0900

    feat: mcore 15.0

--- scripts/qwen_3/mcore_to_hf_qwen3-8b_mcore_v0.15.0.sh
+++ scripts/qwen_3/mcore_to_hf_qwen3-8b_mcore_v0.15.0.sh
@@ -2,12 +2,11 @@
 #PBS -q rt_HF
 #PBS -N megatron-to-hf
 #PBS -l select=1:ncpus=192:ngpus=8
-#PBS -l walltime=1:00:00
+#PBS -l walltime=0:30:00
 #PBS -j oe
 #PBS -m n
 #PBS -v USE_SSH=1
 #PBS -koed
-#PBS -V
 #PBS -o outputs/convert/megatron-to-hf/
 
 cd $PBS_O_WORKDIR
@@ -16,15 +15,7 @@ mkdir -p outputs/convert/megatron-to-hf/
 echo "Nodes allocated to this job:"
 cat $PBS_NODEFILE
 
-source /etc/profile.d/modules.sh
-module use /home/acf15649kv/modules/modulefiles
-
-module load cuda/12.9.1
-module load cudnn/9.10.2
-module load nccl/2.27.5-cuda12.9
-module load hpcx/2.23.0
-
-ITERATIONS=(500 2000 4000 8000 10000 12500)
+ITERATIONS=(10)
 
 for ITERATION in "${ITERATIONS[@]}"; do
   FORMATTED_ITERATION="$(printf "%07d" "${ITERATION}")"
@@ -32,8 +23,8 @@ for ITERATION in "${ITERATIONS[@]}"; do
 
   # model config
   HF_CHECKPOINT_DIR=/groups/gag51395/hf_checkpoints/Qwen3-8B-Base
-  MEGATRON_CHECKPOINT_DIR=/groups/gch51639/fujii/checkpoints/Qwen-3-Swallow-8B-swallow-corpus-v3/exp4/tp2-pp1-ct1/LR1.50E-5-MINLR1.50E-6-WD0.1/iter_${FORMATTED_ITERATION}
-  HF_CHECKPOINT_SAVE_DIR=/groups/gch51639/fujii/checkpoints/megatron-to-hf/Qwen3-Swallow-8B-swallow-corpus-v3/v3.2-gpt-oss-guard-top15/LR1.50E-5-MINLR1.50E-6-WD0.1/iteration_${FORMATTED_ITERATION}
+  MEGATRON_CHECKPOINT_DIR=/groups/gch51639/fujii/checkpoints/Qwen-3-Swallow-8B-v0.2/tp2-pp1-ct1/LR1.50E-5-MINLR1.50E-6-WD0.1//iter_${FORMATTED_ITERATION}
+  HF_CHECKPOINT_SAVE_DIR=/groups/gch51639/fujii/checkpoints/megatron-to-hf/Qwen-3-Swallow-8B-v0.2/tp2-pp1-ct1/LR1.50E-5-MINLR1.50E-6-WD0.1/iteration_${FORMATTED_ITERATION}
   mkdir -p "${HF_CHECKPOINT_SAVE_DIR}"
 
   # skip if megatron checkpoint not found
@@ -43,7 +34,7 @@ for ITERATION in "${ITERATIONS[@]}"; do
   fi
 
   export CUDA_DEVICE_MAX_CONNECTIONS=1
-  MEGATRON_LM_PATH=/home/acf15649kv/src/Megatron-LM-v0.14.0rc7
+  MEGATRON_LM_PATH=/home/acf15649kv/src/Megatron-LM-v0.15.0
   MEGATRON_BRIDGE_PATH=$(pwd)/src
   export PYTHONPATH="$PYTHONPATH:$MEGATRON_LM_PATH:$MEGATRON_BRIDGE_PATH"
 
