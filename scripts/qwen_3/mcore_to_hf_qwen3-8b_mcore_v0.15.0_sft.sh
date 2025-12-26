89cf7bf85aea43aed3dd7d2a7241d5c10361cea7
kazuki <kaz.tokyo.tech20@gmail.com>
2025-12-27 06:42:34 +0900

    feat: convert sft checkpoint

--- scripts/qwen_3/mcore_to_hf_qwen3-8b_mcore_v0.15.0_sft.sh
+++ scripts/qwen_3/mcore_to_hf_qwen3-8b_mcore_v0.15.0_sft.sh
@@ -16,17 +16,7 @@ mkdir -p outputs/convert/megatron-to-hf/
 echo "Nodes allocated to this job:"
 cat $PBS_NODEFILE
 
-source /etc/profile.d/modules.sh
-module use /home/acf15649kv/modules/modulefiles
-
-module load cuda/12.9.1
-module load cudnn/9.10.2
-module load nccl/2.27.5-cuda12.9
-module load hpcx/2.23.0
-
-source /home/acf15649kv/src/Megatron-LM-v0.13.0rc2/.venv/bin/activate
-
-ITERATIONS=(1000 2500 5000 12000)
+ITERATIONS=(7800)
 
 for ITERATION in "${ITERATIONS[@]}"; do
   FORMATTED_ITERATION="$(printf "%07d" "${ITERATION}")"
@@ -34,8 +24,8 @@ for ITERATION in "${ITERATIONS[@]}"; do
 
   # model config
   HF_CHECKPOINT_DIR=/groups/gag51395/hf_checkpoints/Qwen3-8B
-  MEGATRON_CHECKPOINT_DIR=/groups/gch51639/fujii/checkpoints/Qwen3-8B-SFT-ablation/exp5/LR1.50E-5-MINLR1.50E-6-WD0.1/iter_${FORMATTED_ITERATION}
-  HF_CHECKPOINT_SAVE_DIR=/groups/gch51639/fujii/checkpoints/megatron-to-hf/Qwen3-8B-SFT-ablation/exp5/LR1.50E-5-MINLR1.50E-6-WD0.1/iteration_${FORMATTED_ITERATION}
+  MEGATRON_CHECKPOINT_DIR=/groups/gch51639/fujii/checkpoints/Qwen3-8B-SFT-ablation/exp11/LR1.50E-5-MINLR1.50E-6-WD0.1/iter_${FORMATTED_ITERATION}
+  HF_CHECKPOINT_SAVE_DIR=/groups/gch51639/fujii/checkpoints/megatron-to-hf/Qwen3-8B-SFT-ablation/exp11/LR1.50E-5-MINLR1.50E-6-WD0.1/iteration_${FORMATTED_ITERATION}
   mkdir -p "${HF_CHECKPOINT_SAVE_DIR}"
 
   # skip if megatron checkpoint not found
@@ -45,11 +35,20 @@ for ITERATION in "${ITERATIONS[@]}"; do
   fi
 
   export CUDA_DEVICE_MAX_CONNECTIONS=1
-  MEGATRON_LM_PATH=/home/acf15649kv/src/Megatron-LM-v0.13.0rc2
+  MEGATRON_LM_PATH=/home/acf15649kv/src/Megatron-LM-v0.15.0
   MEGATRON_BRIDGE_PATH=$(pwd)/src
   export PYTHONPATH="$PYTHONPATH:$MEGATRON_LM_PATH:$MEGATRON_BRIDGE_PATH"
 
   # convert
+  singularity exec \
+  --nv \
+  --bind /groups/gag51395:/groups/gag51395 \
+  --bind /groups/gch51639:/groups/gch51639 \
+  --bind /home/acf15649kv:/home/acf15649kv \
+  --bind /dev/shm:/dev/shm \
+  --bind /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem:/etc/ssl/certs/ca-certificates.crt \
+  --bind /tmp:/tmp \
+  /groups/gch51639/fujii/container/ngc-pytorch-25.10.sif \
   python examples/models/checkpoint_conversion.py export \
     --hf-model "${HF_CHECKPOINT_DIR}" \
     --megatron-path "${MEGATRON_CHECKPOINT_DIR}" \
